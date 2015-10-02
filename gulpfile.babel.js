86ecb99c3d812719e20be39e825665849bb404f5
Rusty Rountree <rustonrountree@gmail.com>
2015-10-01 19:56:03 -0500

    Stub in some more gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,3 +7,15 @@ gulp.task('default', () => {
 gulp.task('minify', () => {
     console.log("I'm minifying!");
 });
+
+gulp.task('images', () => {
+    console.log("I'm optimizing images!");
+});
+
+gulp.task('styles', () => {
+    console.log("I'm processing stylesheets!");
+});
+
+gulp.task('html', () => {
+    console.log("I'm processing html!");
+});
